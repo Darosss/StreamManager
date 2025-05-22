1a3b3a0c661bc38aaa79a91fb19e7b1b272c27cd
darosss <darosss12@proton.me>
2025-05-23 00:25:00 +0200

    chore(fe/vite/scss): update paths for new scss v

--- frontend/vite.config.ts
+++ frontend/vite.config.ts
@@ -3,13 +3,17 @@ import react from "@vitejs/plugin-react";
 import viteTsconfigPaths from "vite-tsconfig-paths";
 import svgrPlugin from "vite-plugin-svgr";
 import eslint from "vite-plugin-eslint";
+import path from "path";
 
 export default defineConfig({
   css: {
     preprocessorOptions: {
-      scss: {
-        additionalData: `@use "./src/_variables.scss" as *;`,
-      },
+      scss: { additionalData: `@use "src/_variables.scss" as *;` },
+    },
+  },
+  resolve: {
+    alias: {
+      src: path.resolve(__dirname, "./src"),
     },
   },
   plugins: [react(), viteTsconfigPaths(), svgrPlugin(), eslint()],
