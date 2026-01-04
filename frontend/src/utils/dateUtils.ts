e751abf334d878d2458fe30ff6abc5a13120c4dd
Darosss <darosss12@proton.me>
2026-01-04 16:35:35 +0100

    refactor(frontend/utils): move date logic utils into dateUtils file

--- frontend/src/utils/dateUtils.ts
+++ frontend/src/utils/dateUtils.ts
@@ -1,5 +1,14 @@
 import dayjs from "./utils";
 
+export const convertSecondsToMS = (secondsConvert: number) => {
+  const duration = dayjs.duration(secondsConvert, "seconds");
+  const minutes = Math.floor(duration.asMinutes());
+  const seconds = (Math.floor(duration.asSeconds()) % 60)
+    .toString()
+    .padStart(2, "0");
+  return [minutes, seconds];
+};
+
 export const getDateFromSecondsToYMDHMS = (time: number, separator = " ") => {
   const duration = dayjs.duration(time, "seconds");
 
