ee2183f8a725ea147adce3f728228281c9b731be
Darosss <darosss12@proton.me>
2026-01-14 18:31:35 +0100

    feat(frontend/notifications): add initial notifications context

---
+++ frontend/src/contexts/notifications/enums.ts
@@ -0,0 +1,22 @@
+export enum NOTIFICATION_CONTAINER {
+  BOTTOM_LEFT = "bottom-left",
+  BOTTOM_RIGHT = "bottom-right",
+  BOTTOM_CENTER = "bottom-center",
+  TOP_LEFT = "top-left",
+  TOP_RIGHT = "top-right",
+  TOP_CENTER = "top-center",
+  CENTER = "center",
+  TOP_FULL = "top-full",
+  BOTTOM_FULL = "bottom-full",
+}
+export enum NOTIFICATION_INSERT {
+  TOP = "top",
+  BOTTOM = "bottom",
+}
+export enum NOTIFICATION_TYPE {
+  SUCCESS = "success",
+  DANGER = "danger",
+  INFO = "info",
+  DEFAULT = "default",
+  WARNING = "warning",
+}
---
+++ frontend/src/contexts/notifications/index.ts
@@ -0,0 +1,19 @@
+import "./styles.scss";
+import {
+  NOTIFICATION_CONTAINER,
+  NOTIFICATION_INSERT,
+  NOTIFICATION_TYPE,
+} from "./enums";
+import {
+  NotificationsContextProvider,
+  useNotifications,
+} from "./NotificationsContext";
+import { NotificationOpts } from "./types";
+export {
+  NotificationsContextProvider,
+  useNotifications,
+  NOTIFICATION_CONTAINER,
+  NOTIFICATION_INSERT,
+  NOTIFICATION_TYPE,
+};
+export type { NotificationOpts };
