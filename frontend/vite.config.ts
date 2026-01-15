19c248d9f2bdca9bf9828634d25e58c449de4679
Darosss <darosss12@proton.me>
2026-01-15 17:32:50 +0100

    chore(frontend/packages): update eslint, its plugins, configs & configs

--- frontend/vite.config.ts
+++ frontend/vite.config.ts
@@ -2,7 +2,7 @@ import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
 import viteTsconfigPaths from "vite-tsconfig-paths";
 import svgrPlugin from "vite-plugin-svgr";
-import eslint from "vite-plugin-eslint";
+import eslint from "@nabla/vite-plugin-eslint";
 import path from "path";
 import { visualizer } from "rollup-plugin-visualizer";
 import { playwright } from "@vitest/browser-playwright";
