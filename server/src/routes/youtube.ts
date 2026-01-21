0fcd8d780aa7c35e31bc9f96a11d4b26d9da0df3
Darosss <darosss12@proton.me>
2026-01-22 00:33:52 +0100

    refactor(server): remove youtube / ytdl from routes & controllers

--- server/src/routes/youtube.ts
+++
@@ -1,8 +0,0 @@
-import { Router } from "express";
-import { streamYoutubeVideo } from "@controllers";
-
-const youtubeRouter = Router();
-
-youtubeRouter.get("/videos/:id", streamYoutubeVideo);
-
-export default youtubeRouter;
