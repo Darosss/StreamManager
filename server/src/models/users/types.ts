2606961e2c5f08e8eeef133ba742fbe2eec13160
Darosss <darosss12@proton.me>
2026-02-08 14:29:43 +0100

    feat(server/models/users): add generic UserModel type for displayBadges

--- server/src/models/users/types.ts
+++ server/src/models/users/types.ts
@@ -2,9 +2,9 @@ import { Document } from "mongoose";
 import { BaseModel } from "../types";
 import { BadgeModel } from "../badges";
 
-export type UserDisplayBadgesType = [BadgeModel, BadgeModel, BadgeModel];
+export type UserDisplayBadgesType<BadgeType extends string | BadgeModel = string> = [BadgeType, BadgeType, BadgeType];
 
-export interface UserModel extends BaseModel {
+export interface UserModel<BadgeType extends string | BadgeModel = string> extends BaseModel {
   twitchId: string;
   username: string;
   privileges: number;
@@ -16,7 +16,7 @@ export interface UserModel extends BaseModel {
   twitchName?: string;
   twitchCreated?: Date;
   follower?: Date;
-  displayBadges?: UserDisplayBadgesType;
+  displayBadges?: UserDisplayBadgesType<BadgeType>;
 }
 
 export type UserDocument = UserModel & Document;
