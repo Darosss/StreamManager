3778bf702ce3f34a8b674197734e52a8f073027c
Darosss <darosss12@proton.me>
2026-05-20 00:29:25 +0200

    refactor(frontend/hooks): remove useAxiosWithConfirmation

--- frontend/src/hooks/index.ts
+++ frontend/src/hooks/index.ts
@@ -1,7 +1,6 @@
 export { useFileUpload } from "./useFileUpload";
 export { useLocalStorage } from "./useLocalStorage";
 export { usePagination, DOTS } from "./usePagination";
-export { useAxiosWithConfirmation } from "./useAxiosHookWithConfirmation";
 export { useGetAllModes } from "./useGetListModes";
 export {
   useCustomMutation,
