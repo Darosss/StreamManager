1edc44b17f5147b436bb9e1b4e6b496aec5bb386
Darosss <darosss12@proton.me>
2026-07-11 12:04:07 +0200

    refactor(server/tests): remove Babel dependencies and update Jest to version 29
    - remove Babel-related packages from devDependencies in package.json.
    - update Jest and added jest-util and ts-jest to devDependencies.
    - add a new mock setup file for Jest tests to mock external dependencies.
    - update tsconfig.json to include isolatedModules for better module handling.

--- server/jest.config.js
+++ server/jest.config.js
@@ -1,14 +1,44 @@
+/* eslint-disable @typescript-eslint/no-require-imports */
 /** @type {import('jest').Config} */
+const { pathsToModuleNameMapper } = require("ts-jest");
+const { compilerOptions } = require("./tsconfig.json");
+module.exports = {
+  projects: [
+    {
+      displayName: "unit",
+      preset: "ts-jest/presets/default-esm",
 
-const config = {
-  testEnvironment: "node",
-  transform: {
-    "^.+\\.(ts|js)$": "babel-jest"
-  },
-  transformIgnorePatterns: ["node_modules/(?!(@twurple)/)"],
-  testPathIgnorePatterns: ["/node_modules/", "/dist/"],
-  moduleFileExtensions: ["ts", "js", "json"],
-  clearMocks: true
+      testEnvironment: "node",
+      transform: {
+        "^.+\\.(ts|js)$": ["ts-jest", { useESM: true, tsconfig: "tsconfig.json" }]
+      },
+
+      clearMocks: true,
+      moduleFileExtensions: ["ts", "js", "json"],
+      testPathIgnorePatterns: ["/node_modules/", "/dist/"],
+      testMatch: ["**/unit/*.test.ts"],
+      moduleNameMapper: pathsToModuleNameMapper(compilerOptions.paths, {
+        prefix: "<rootDir>/"
+      }),
+      setupFilesAfterEnv: ["<rootDir>/tests/mock.setup.js"]
+    },
+    {
+      displayName: "integration",
+      preset: "ts-jest/presets/default-esm",
+
+      testEnvironment: "node",
+      transform: {
+        "^.+\\.(ts|js)$": ["ts-jest", { useESM: true, tsconfig: "tsconfig.json" }]
+      },
+
+      clearMocks: true,
+      moduleFileExtensions: ["ts", "js", "json"],
+      testPathIgnorePatterns: ["/node_modules/", "/dist/"],
+      testMatch: ["**/integration/*.test.ts"],
+      moduleNameMapper: pathsToModuleNameMapper(compilerOptions.paths, {
+        prefix: "<rootDir>/"
+      }),
+      setupFilesAfterEnv: ["<rootDir>/tests/mock.setup.js"]
+    }
+  ]
 };
-
-module.exports = config;
